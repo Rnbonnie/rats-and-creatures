a171aac6dd681d41ada2878b889d72173211faa4
Rnbonnie <minnahmetowartur@gmail.com>
2026-01-29 10:30:11 +0300

    Изменения файлов сборки и добавление ассетов

--- src/main/java/com/rnoobb/rats/RatsAndCreatures.java
+++ src/main/java/com/rnoobb/rats/RatsAndCreatures.java
@@ -5,20 +5,20 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
+import net.minecraft.item.FoodComponent;
+
 public class RatsAndCreatures implements ModInitializer {
-	public static final String MOD_ID = "rats-and-creatures";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final String MOD_ID = "rats_and_creatures";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
+public static final FoodComponent SUSPICIOUS_FOOD_COMPONENT = new FoodComponent.Builder()
+		.hunger(3)
+        .saturationModifier(0.3f)
+        .build();
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+	// In your item registration class
 
-		LOGGER.info("Hello Fabric world!");
+        LOGGER.info("Hello Fabric world!");
 	}
 }
