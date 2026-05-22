4c81c80043787a9653ecef0e50e0c0dbb4693983
Rnbonnie <minnahmetowartur@gmail.com>
2026-05-22 07:06:37 +0300

    Чистка от мусора

--- get_class_hierarchy.py
+++
@@ -1,2 +0,0 @@
-import subprocess
-print(subprocess.getoutput("javap -classpath $(find ~/.gradle/caches/fabric-loom/ -name '*.jar' | grep -i 'minecraft-1.20' | head -n 1) net.minecraft.client.render.entity.model.BatEntityModel"))
