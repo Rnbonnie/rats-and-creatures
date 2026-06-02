f9ab7b63258e4adc2e1c67b47b049b5d2afac5d7
Rnbonnie <minnahmetowartur@gmail.com>
2026-06-02 19:23:05 +0300

    Изменение текстур

--- src/client/java/com/rnoobb/rats/RatsAndCreaturesClient.java
+++ src/client/java/com/rnoobb/rats/RatsAndCreaturesClient.java
@@ -6,11 +6,14 @@ import com.rnoobb.rats.client.renderer.BatCompanionRenderer;
 import com.rnoobb.rats.client.renderer.RavenRenderer;
 import com.rnoobb.rats.client.screen.RatScreen;
 import com.rnoobb.rats.entity.ModEntities;
+import com.rnoobb.rats.item.CageItem;
 import com.rnoobb.rats.screen.ModScreenHandlers;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
 import net.minecraft.client.gui.screen.ingame.HandledScreens;
+import net.minecraft.client.item.ModelPredicateProviderRegistry;
+import net.minecraft.util.Identifier;
 
 public class RatsAndCreaturesClient implements ClientModInitializer {
 	@Override
@@ -19,5 +22,8 @@ public class RatsAndCreaturesClient implements ClientModInitializer {
 		EntityRendererRegistry.register(ModEntities.RAT, RatRenderer::new);
 		EntityRendererRegistry.register(ModEntities.RAVEN, RavenRenderer::new);
         HandledScreens.register(ModScreenHandlers.RAT_SCREEN_HANDLER, RatScreen::new);
+
+        ModelPredicateProviderRegistry.register(ModItems.CAGE, new Identifier(RatsAndCreatures.MOD_ID, "filled"),
+                (stack, world, entity, seed) -> CageItem.hasStoredEntity(stack) ? 1f : 0f);
 	}
 }
